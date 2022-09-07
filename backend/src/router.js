6788ad3b7d7928cd2578af69d07eb4010ee13150
loulou8011984D <louptaxi@gmail.com>
2022-09-07 16:50:07 +0200

    add routers

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,9 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
+const ticketControllers = require("./controllers/ticketControllers");
+const trainControllers = require("./controllers/trainControllers");
 
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
+router.get("/ticket", ticketControllers.browse);
+router.get("/ticket/:id", ticketControllers.read);
+router.get("/train", trainControllers.browse);
+router.get("/train/:id", trainControllers.read);
+
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
