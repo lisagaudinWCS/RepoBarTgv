88997f6e129f064eafd4ea42f94554b12a8d9a34
laurentsata <sataward666@gmail.com>
2022-09-12 14:55:42 +0200

    creat nav bar without css

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+// import {Link} from 'react-router-dom';
 
 export default function NavBar() {
   return (
-    <div className="navBar">
-      <h4>navBar of the dead</h4>
+    <div>
+      <nav className="navbar">
+        <ul className="navbar-ul">
+          <li>{/* <Link to="/">Home</Link> */}</li>
+          <li>{/* <Link to="/advanced-search">Search</Link> */}</li>
+          <li>{/* <Link to="/random">Random Game</Link> */}</li>
+          <li>{/* <Link to="/quiz">Quiz</Link> */}</li>
+          <li>{/* <Link to="/indexgames">Index</Link> */}</li>
+          <li>{/* <Link to="/glossary">Glossary</Link> */}</li>
+        </ul>
+      </nav>
     </div>
   );
 }
