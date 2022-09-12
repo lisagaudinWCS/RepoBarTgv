ab9d5b9006b549613f3ffa9336b4a3b3ac619dc9
laurentsata <sataward666@gmail.com>
2022-09-12 11:16:32 +0200

    fix indentations

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function NavBar() {
   return (
     <div className="navBar">
-      <h2>navBar of the dead</h2>
+      <h4>navBar of the dead</h4>
     </div>
   );
 }
