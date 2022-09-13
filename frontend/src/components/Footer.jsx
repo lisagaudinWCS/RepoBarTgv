4bf3def6c88d29ae6b38224c67df327c3f3aa33f
laurentsata <sataward666@gmail.com>
2022-09-13 16:15:05 +0200

    background color black & lien sncf =>Footer.jsx, css

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
   return (
     <div className="footer">
-      <p>footer de merde</p>
+      <div className="sncfConnect">
+        <a href="https://www.sncf-connect.com/">www.sncf-connect.com</a>{" "}
+        <p>test</p>
+      </div>
+      <div>
+        <p>test</p>
+      </div>
     </div>
   );
 }
