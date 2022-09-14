6326297f2284f135cecbd44fc16212c5224401fc
laurentsata <sataward666@gmail.com>
2022-09-14 16:59:01 +0200

    px==>% vh & footer ... responsive ===0

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,15 +3,17 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <div className="sncfConnect">
-        <a href="https://www.sncf-connect.com/">www.sncf-connect.com</a>{" "}
-      </div>
-      <div className="logoSncf">
-        <img
-          src="https://res.cloudinary.com/dbkscupri/image/upload/v1663145083/logo%20sncfp3/logoSncf_uhcg4l.png"
-          alt="logo sncf"
-        />
+    <div className="blocFooter">
+      <div className="footer">
+        <div className="sncfConnect">
+          <a href="https://www.sncf-connect.com/">www.sncf-connect.com</a>{" "}
+        </div>
+        <div className="logoSncf">
+          <img
+            src="https://res.cloudinary.com/dbkscupri/image/upload/v1663145083/logo%20sncfp3/logoSncf_uhcg4l.png"
+            alt="logo sncf"
+          />
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from "@components/Header";
 import Footer from "@components/Footer";
 import NavBar from "@components/NavBar";
 import Ban from "@components/Ban";
+import "./Home.css";
 
 export default function Home() {
   return (
