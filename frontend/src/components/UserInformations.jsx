667b927976f24a5317f183b7d37f1fa8b02a4a01
lisagaudinWCS <lisagaudin.wcs@gmail.com>
2022-09-15 12:11:37 +0200

    updating dev

--- frontend/src/components/UserInformations.jsx
+++ frontend/src/components/UserInformations.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function UserInformations() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const [clientInfos, setClientInfos] = useState({
     // client_number: 101,
@@ -24,77 +25,86 @@ export default function UserInformations() {
   useEffect(() => getClientInfos(), []);
 
   return (
-    <div className="profile-container">
-      <div className="avatar-container">
-        <img
-          src={clientInfos.avatar}
-          alt="avatar"
-          className="avatar"
-          //   alt={`avatar of ${client.name}`}
-        />
-        <button type="button" className="avatar-btn" id="avatar-btn">
-          Modifier image
-        </button>
-      </div>
-      {/* <div className="info-container" /> */}
-      <div className="user-input-box">
-        <p className="change-info-btn">MODIFIER MES INFOS ✏️</p>
+    <>
+      <h1 className="profile-title">Mon profil</h1>
+      <div className="profile-container">
+        <div className="avatar-container">
+          <img
+            src={clientInfos.avatar}
+            // alt="avatar"
+            className="avatar"
+            alt={`avatar of ${clientInfos.name}`}
+          />
+          <button type="button" className="avatar-btn" id="avatar-btn">
+            Modifier image
+          </button>
+        </div>
+        {/* <div className="info-container" /> */}
+        <div className="user-input-box">
+          <button
+            type="button"
+            className="change-info-btn"
+            onClick={() => navigate(`/profil/${id}/edit`)}
+          >
+            MODIFIER MES INFOS ✏️
+          </button>
 
-        <label className="user-label" htmlFor="lastname">
-          Nom :
-        </label>
-        <input
-          className="user-input"
-          type="text"
-          id="lastname"
-          value={clientInfos.lastname}
-          disabled
-        />
-        <br />
-        <label className="user-label" htmlFor="firstname">
-          Prénom :
-        </label>
-        <input
-          className="user-input"
-          type="text"
-          id="firstname"
-          value={clientInfos.firstname}
-          disabled
-        />
-        <br />
-        <label className="user-label" htmlFor="birth_date">
-          Date de naissance :
-        </label>
-        <input
-          className="user-input"
-          type="date"
-          id="birth_date"
-          value={clientInfos.birth_date}
-          disabled
-        />
-        <br />
-        <label className="user-label" htmlFor="email">
-          E-mail :
-        </label>
-        <input
-          className="user-input"
-          type="email"
-          id="email"
-          value={clientInfos.email}
-          disabled
-        />
-        <br />
-        <label className="user-label" htmlFor="pass">
-          Mot de passe :
-        </label>
-        <input
-          type="password"
-          id="password"
-          value={clientInfos.password}
-          disabled
-        />
-        <br />
+          <label className="user-label" htmlFor="lastname">
+            Nom :
+          </label>
+          <input
+            className="user-input"
+            type="text"
+            id="lastname"
+            value={clientInfos.lastname}
+            disabled
+          />
+          <br />
+          <label className="user-label" htmlFor="firstname">
+            Prénom :
+          </label>
+          <input
+            className="user-input"
+            type="text"
+            id="firstname"
+            value={clientInfos.firstname}
+            disabled
+          />
+          <br />
+          <label className="user-label" htmlFor="birth_date">
+            Date de naissance :
+          </label>
+          <input
+            className="user-input"
+            type="date"
+            id="birth_date"
+            value={clientInfos.birth_date}
+            disabled
+          />
+          <br />
+          <label className="user-label" htmlFor="email">
+            E-mail :
+          </label>
+          <input
+            className="user-input"
+            type="email"
+            id="email"
+            value={clientInfos.email}
+            disabled
+          />
+          <br />
+          <label className="user-label" htmlFor="pass">
+            Mot de passe :
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={clientInfos.password}
+            disabled
+          />
+          <br />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
