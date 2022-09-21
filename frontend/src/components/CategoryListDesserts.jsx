28ec63c3a8afc5d97d49231941c5a4b19356839f
NDook <95679356+NDook@users.noreply.github.com>
2022-09-21 09:47:21 +0200

    Update CategoryListDesserts.jsx

--- frontend/src/components/CategoryListDesserts.jsx
+++ frontend/src/components/CategoryListDesserts.jsx
@@ -8,7 +8,7 @@ function CategoryListDesserts() {
 
   const getProducts = () => {
     axios
-      .get("http://localhost:5000/categories/4")
+      .get(`${import.meta.env.VITE_BACKEND_URL}/categories/4`)
       .then((response) => response.data)
       .then((data) => setProducts(data));
   };
