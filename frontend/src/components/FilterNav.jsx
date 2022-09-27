451b090d459811fef50a8db297b7b59196b524e1
NDook <thomasmludek@gmail.com>
2022-09-27 10:48:10 +0200

    optimize responsive

--- frontend/src/components/FilterNav.jsx
+++ frontend/src/components/FilterNav.jsx
@@ -19,6 +19,7 @@ export default function FilterNav({ setfilterNavigation }) {
     <div>
       <div className={fix ? "filter-nav fixed" : "filter-nav"}>
         <button
+          className="container-home-icon"
           type="button"
           value="
           0"
