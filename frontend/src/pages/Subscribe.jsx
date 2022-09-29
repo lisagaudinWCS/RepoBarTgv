d2dba0c12e359dc5617a9fc84a15ec2cbb668f42
lisagaudinWCS <lisagaudin.wcs@gmail.com>
2022-09-29 11:08:29 +0200

    deleting bannierehome cmpt on login/subscribe/signin pages

--- frontend/src/pages/Subscribe.jsx
+++ frontend/src/pages/Subscribe.jsx
@@ -1,10 +1,8 @@
-import BanniereHome from "../components/BanniereHome";
 import SubscribeForm from "../components/SubscribeForm";
 
 function Subscribe() {
   return (
     <div>
-      <BanniereHome />
       <h3 className="subscribe-title-red">INSCRIPTION</h3>
       <SubscribeForm />
     </div>
