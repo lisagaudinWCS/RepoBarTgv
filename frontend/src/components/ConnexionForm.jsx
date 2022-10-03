c79a349b090467be1ef0b38261d8ea8aa7170180
NDook <thomasmludek@gmail.com>
2022-10-03 11:37:10 +0200

    perform identification

--- frontend/src/components/ConnexionForm.jsx
+++ frontend/src/components/ConnexionForm.jsx
@@ -3,17 +3,9 @@ import { useNavigate } from "react-router-dom";
 import AuthAPI from "../services/AuthAPI";
 import AuthContext from "../contexts/AuthContext";
 import "./connexion.css";
-// import jwtDecode from "jwt-decode";
-
-// const jwt = require("jsonwebtoken");
-// // this is just for test, get a real verified token from your middleware
-// const testToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
-// const token = jwt.decode(testToken);
-// let subjectId = token.sub;
 
 export default function ConnexionForm() {
   const navigate = useNavigate();
-  // const token = jwtDecode(token.sub);
 
   const [users, setUsers] = useState({
     email: "",
@@ -27,7 +19,8 @@ export default function ConnexionForm() {
     try {
       await AuthAPI.authenticate(users);
       setIsAuthenticated(true);
-      navigate(`/profil/1`);
+      navigate(`/`);
+      window.location.reload();
     } catch (err) {
       console.error(err);
     }
